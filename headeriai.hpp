e0f98e8f3ebec4d2b2196ddab87d7e9cf0a9bfe9
Mantas Mačiukas <60518314+MantasM2001@users.noreply.github.com>
2021-09-10 14:21:55 +0300

    Adding another function

--- headeriai.hpp
+++ headeriai.hpp
@@ -4,5 +4,6 @@
 
 using namespace std;
 
+string VartotojoIvedimas();
 bool FailoTikrinimas(string failas);
 string Hashinimas(bool ivedimas, string duomenys); //jei duomenys imami is failo, tai kintamasis duomenys laikys failo pavadinima
