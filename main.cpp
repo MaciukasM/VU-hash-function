a188bc06a199f2406bcbb0ef78e80500fa486433
Mantas Mačiukas <60518314+MantasM2001@users.noreply.github.com>
2021-09-10 14:05:40 +0300

    Adding basic user input

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include "headeriai.hpp"
+
+int main()
+{
+    string ats;
+    string failas;
+    string duomenys;
+    string hashas;
+
+    while(ats!="y" && ats!="n")
+    {
+        cout<<"Ar duomenis imti is failo? (y/n): ";
+        cin>>ats;
+        if(ats!="y" && ats!="n") cout<<"\nIveskite atsakyma is naujo.\n"<<endl;
+    }
+    if(ats=="y")
+    {
+        cout<<"Iveskite failo pavadinima (pvz. failas.txt): ";
+        cin>>failas;
+        if(!FailoTikrinimas(failas))
+        {
+            cout<<"Tokio failo nerasta. Programos darbas baigiamas."<<endl;
+            return 0;
+        }
+        hashas = Hashinimas(true, failas);
+    }
+    else 
+    {
+        cout<<"Iveskite duomenis ranka: ";
+        cin>>duomenys;
+        hashas = Hashinimas(false, duomenys);
+    }
+
+    cout<<hashas<<endl;
+}
